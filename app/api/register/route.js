5e36838d9c6ad874edd69072903044501b0560c7
wiktorzyca <wiktoria.janaszek@gmail.com>
2023-12-02 06:44:44 +0100

    register fix

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -9,9 +9,15 @@ export async function POST(req) {
     const { email, password, name, surname } = await req.json();
     const hashedPassword = await bcrypt.hash(password, 10);
     let role ="astro"
-    await User.create({ role, email, password: hashedPassword, name: name, surname: surname });
-    return NextResponse.json({ message: "User registered." }, { status: 201 });
+    let userExixts = await User.find({email: email})
+    console.log(userExixts.length)
+    if(userExixts.length==0){
+      await User.create({ role, email, password: hashedPassword, name: name, surname: surname });
+      return NextResponse.json({ message: "User registered." }, { status: 201 });
+    }
+      return NextResponse.json({ message: "User exixts." }, { status: 400 });
   } catch (error) {
+    console.log(error)
     return NextResponse.json(
         { message: "An error occurred while registering the user." },
         { status: 500 }
