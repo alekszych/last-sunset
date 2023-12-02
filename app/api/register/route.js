c12bf8334540de298f91b3262e34cfe9be967ba5
baczek113 <baczek.jakub@gmail.com>
2023-12-02 05:59:31 +0100

    mission finished

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -6,10 +6,10 @@ import bcrypt from "bcryptjs";
 export async function POST(req) {
   try {
     await connectMongoDB();
-    const { email, password } = await req.json();
+    const { email, password, name, surname } = await req.json();
     const hashedPassword = await bcrypt.hash(password, 10);
     let role ="astro"
-    await User.create({ role, email, password: hashedPassword });
+    await User.create({ role, email, password: hashedPassword, name: name, surname: surname });
     return NextResponse.json({ message: "User registered." }, { status: 201 });
   } catch (error) {
     return NextResponse.json(
