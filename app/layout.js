cd60064f56a4fa79fffa1a0f409e79fb9dcba069
Bernardinio040 <bernard.kaluzynski@gmail.com>
2023-12-02 02:14:31 +0100

    header end

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { AuthProvider } from "./Providers";
 import "./globals.css";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
+import {Dosis, Lato} from "next/dist/compiled/@next/font/dist/google";
 
 export const metadata = {
   title: "Last Sunset",
@@ -9,14 +10,20 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-
     return (
     <html lang="en">
+        <head>
+            {/*Dosis 400*/}
+            {/*Lato 600*/}
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+            <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@600&family=Lato&display=swap" rel="stylesheet" />
+        </head>
         <body>
             <Header />
             <AuthProvider>{children}</AuthProvider>
             <Footer />
         </body>
     </html>
-  );
+    );
 }
--- components/header/header.jsx
+++ components/header/header.jsx
@@ -2,12 +2,13 @@
 import style from './header.module.scss';
 import { usePathname } from 'next/navigation'
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header(type) {
     const pathname = usePathname();
 
     let pathnames = pathname.split("/");
-    let pagetype = pathnames[1]; //first slash is before url (/astronaut/my-vitals)
+    let pagetype = pathnames[1]; //first slash is before url (/astronaut/vitals)
 
     switch (pagetype) {
         case("astronaut"):
@@ -16,10 +17,10 @@ export default function Header(type) {
                     <Image src={"/logo.png"} alt={"logo"} width={32} height={32} />
                     <h5>Last sunset</h5>
                     <div className={style.links}>
-                        <p>Dashboard</p>
-                        <p>Vitals</p>
-                        <p>Tasks</p>
-                        <p>Missions</p>
+                        <Link href={"/astronaut/dashboard"}><p>Dashboard</p></Link>
+                        <Link href={"/astronaut/vitals"}><p>Vitals</p></Link>
+                        <Link href={"/astronaut/tasks"}><p>Tasks</p></Link>
+                        <Link href={"/astronaut/missions"}><p>Missions</p></Link>
                     </div>
                 </header>
             );
@@ -27,7 +28,13 @@ export default function Header(type) {
         case("admin"):
             return(
                 <header className={[style.header, style.headerAdmin].join(" ")}>
-                    <h1>admin</h1>
+                    <Image src={"/logo.png"} alt={"logo"} width={32} height={32} />
+                    <h5>Last sunset</h5>
+                    <div className={style.links}>
+                        <Link href={"/admin/tasks"}><p>Tasks</p></Link>
+                        <Link href={"/admin/astronauts"}><p>Astronauts</p></Link>
+                        <Link href={"/admin/missions"}><p>Missions</p></Link>
+                    </div>
                 </header>
             )
 
