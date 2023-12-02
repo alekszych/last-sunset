b2fc3ed166ace817fa9d8e133e8cc5fbb1688a7f
alekszych <Bla132004>
2023-12-02 06:18:52 +0100

    finished page admin tasks

--- app/admin/tasks/page.jsx
+++ app/admin/tasks/page.jsx
@@ -1,23 +1,29 @@
 import Dashboard from "@/components/dashboard/Dashboard"
 import DashboardElement from "@/components/dashboardElement/DashboardElement"
 import Table from "@/components/table/table"
+import style from "./page.module.scss"
+
+const astronauts = [
+    {
+        id: 124214,
+        name: "John Doe",
+        tasks: [
+            {id: 4121321, name: "Task 1", description: "Lorem ipsum dolor", status: "Pending"}
+        ]
+    }
+]
 
 export default function Page() {
     return(
         <Dashboard>
-            <DashboardElement backgroundColor={"#BAB6C1"}>
-                <Table
-                        headings={["name", "description", "status"]}
-                        items={[
-                                {
-                                    id: 21412,
-                                    name: "Task1",
-                                    description: "Task description lorem ipsum",
-                                    status: "Pending"
-                                }
-                        ]}
-                />
-            </DashboardElement>
+            {astronauts.map(astronaut =>
+                <DashboardElement backgroundColor={"#BAB6C1"} key={astronaut.id} additionalClassName={style.element}>
+                    <h2> {astronaut.name} </h2>
+                    <Table
+                        items={astronaut.tasks}
+                    />
+                </DashboardElement>
+            )}
         </Dashboard>
     )
 }
