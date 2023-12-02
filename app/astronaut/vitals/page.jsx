404cadeecfa21255cbc99484c71ec5907c9a3af1
Bernardinio040 <bernard.kaluzynski@gmail.com>
2023-12-02 10:02:44 +0100

    placze

--- app/astronaut/vitals/page.jsx
+++ app/astronaut/vitals/page.jsx
@@ -1,5 +1,12 @@
+import style from './page.modules.scss';
+import Vitals from "@/components/vitals/vitals";
+import Button from "@/components/button/Button";
+
 export default function Page() {
     return(
-        <div>mrr</div>
+        <div>
+            <Vitals title={"Your vitals"} />
+            <Button additionalClassName={style.buttonEx} btnText={"Report current vitals"} href={"/astronaut/vitals"} />
+        </div>
     )
 }
