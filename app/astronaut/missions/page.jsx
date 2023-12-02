733ba34f5a5ce8edbe937640dc7db6d2f9e2895f
baczek113 <baczek.jakub@gmail.com>
2023-12-02 14:32:21 +0100

    panel misji astronaut skonczony

--- app/astronaut/missions/page.jsx
+++ app/astronaut/missions/page.jsx
@@ -1,5 +1,59 @@
+"use client";
+import {useEffect, useState} from "react";
+import Button from "@/components/button/Button";
+import Dashboard from "@/components/dashboard/Dashboard";
+import ProgressBar from "@/components/progressBar/ProgressBar";
+import Table from "@/components/table/table";
+import style from "./page.module.scss";
+
 export default function Page() {
-    return(
-        <div>mrr</div>
-    )
+    const[mission, setMission] = useState(null);
+    const[loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('/api/mission')
+            .then((res) => res.json())
+            .then((data) => {
+                setMission(data.mission[0]);
+                setLoading(false);
+                console.log(data);
+            })
+    }, []);
+
+    if(loading && !mission){
+        return(
+            <div style={{"display":"flex", "justify-content":"center", "align-items":"center", "width":"100%", "height":"100%"}}>
+                <h1>LOADING...</h1>
+            </div>
+        )
+    }
+    else
+    {
+        console.log(mission);
+        return (
+            <div style={{"padding": "64px 0"}}>
+                <Dashboard>
+                    <section className={style.midsectionSection}>
+                        <div className={style.currentlySelectedMoreInfo}>
+                            <h5>
+                                Mission name
+                            </h5>
+                            <h3>
+                                {mission.title}
+                            </h3>
+                        </div>
+                    </section>
+                    <section className={style.midsectionSection}>
+                        <div className={style.currentlySelectedMoreInfoColor2}>
+                            <div className={style.progressBar}>
+                                <h3>Progress</h3>
+                                <ProgressBar tasks={mission.milestones}></ProgressBar>
+                            </div>
+                            <Table items={mission.milestones}></Table>
+                        </div>
+                    </section>
+                </Dashboard>
+            </div>
+        )
+    }
 }
